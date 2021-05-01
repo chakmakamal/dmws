25b670ba44f21e624e04cd88e21fe3c945a99305
kamal chakma <53227040+chakmakamal@users.noreply.github.com>
2021-05-01 08:54:04 +0530

    Add files via upload

---
+++ dmws.js
@@ -0,0 +1,46 @@
+function showMenu(){
+    const x = document.getElementById("dmws_menus");
+    if(x.style.display === "block")
+    {
+        x.style.display = "none";
+    }else{
+        x.style.display = "block";
+    }
+}
+
+function donatePage(){
+
+    const donatePage = document.getElementById("donatePage");
+    if( donatePage.style.display === "block")
+    {
+        donatePage.style.display = "none";
+    }else{
+        donatePage.style.display = "block";
+    }
+}
+const dmwsBody = document.getElementById("main");
+dmwsBody.addEventListener("click", function(){
+    let x = document.getElementById("dmws_menus");
+    if( x.style.display === "block")
+    {
+        x.style.display = "none";
+    }
+})
+// dmwsBody.addEventListener("click", function(){
+//     const donatePage= document.getElementById("donatePage");
+//     if(donatePage.style.display === "block")
+//     {
+//         donatePage.style.display = "none";
+//     }
+// })
+
+//close donate page function
+const closeDonatePage = document.getElementById("closeDonatePage");
+
+closeDonatePage.addEventListener("click",function(){
+    const donatePage = document.getElementById("donatePage");
+    if( donatePage.style.display === "block")
+    {
+        donatePage.style.display = "none";
+    }
+})
